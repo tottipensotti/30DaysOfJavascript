4a861c01f9be12013c0324cc02f48d9916697dce
Agustín Pensotti <91265440+tottipensotti@users.noreply.github.com>
2022-02-17 17:39:24 -0300

    Update script.js

--- 02 - Analog Clock/script.js
+++ 02 - Analog Clock/script.js
@@ -10,11 +10,11 @@ function setDate(){
     secHand.style.transform = `rotate(${secondsDegrees}deg)`;
     
     const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
     minHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
     const hours = now.getHours();
-    const hoursDegrees = ((hours / 60) * 360) + 90;
+    const hoursDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
     hsHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
